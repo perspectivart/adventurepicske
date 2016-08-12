b8b3ee98d20c88be1c02bd240a9521b6a9319874
Don Xihote <sydneysayeed@gmail.com>
2016-08-12 13:55:18 +0300

    all models complete

--- database/migrations/2016_08_12_075912_allmigrations.php
+++ database/migrations/2016_08_12_075912_allmigrations.php
@@ -25,7 +25,7 @@ class Allmigrations extends Migration
 			$table->increments("id");
 			$table->integer("user_id")->unsigned();
 			$table->foreign("user_id")->references("id")->on("users");
-			$table->interger("usertype");
+			$table->integer("usertype");
 			$table->timestamps();
 			
 			
